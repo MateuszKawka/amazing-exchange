880585f900e4d3c116779e5b6be890027a41bf2f
KawkaMateusz <kaawkamateusz@gmail.com>
2019-04-02 21:58:20 +0200

    Initial dist subtree commit

---
+++ precache-manifest.08ef2b6f130582be26dfd111aaa54309.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/amazing-exchange/robots.txt"
+  },
+  {
+    "revision": "d3bd714455d4a6745ba5",
+    "url": "/amazing-exchange/js/chunk-vendors.a123aa27.js"
+  },
+  {
+    "revision": "54b97f90a8ecf62c3d19",
+    "url": "/amazing-exchange/js/app.a8329725.js"
+  },
+  {
+    "revision": "54be67041727f75fbffd",
+    "url": "/amazing-exchange/js/about.68cd6bdd.js"
+  },
+  {
+    "revision": "d7d785703caa221a6364069913e5813a",
+    "url": "/amazing-exchange/index.html"
+  },
+  {
+    "revision": "d3bd714455d4a6745ba5",
+    "url": "/amazing-exchange/css/chunk-vendors.71d40652.css"
+  },
+  {
+    "revision": "54be67041727f75fbffd",
+    "url": "/amazing-exchange/css/about.87a5b555.css"
+  }
+];
